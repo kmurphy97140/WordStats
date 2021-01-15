c058a876f7e787fc198c2f333f865c405b9b0f60
Kevin Murphy <kmurphy@pacstar.com>
2021-01-15 09:08:42 -0800

    Initial count of words

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace WordStats
+{
+	// Gathers some stats based on the word list from https://github.com/dwyl/english-words.  
+	// Note: words_alpha.txt (included in this solution) is not mine; it comes from the link above.
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var words = File.ReadAllLines("words_alpha.txt");
+			int allWordCount = words.Length;
+			int fourLetterWordCount = 0;
+
+			fourLetterWordCount = words.Count(word => word.Length == 4);
+
+			Console.WriteLine($"Number of all words: {allWordCount}  four letter words: {fourLetterWordCount}");
+		}
+	}
+}
